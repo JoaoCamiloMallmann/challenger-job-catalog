448e2570419e698ee37de2f029141450e99d92ea
JC <jcamilomallmann@hotmail.com>
2025-02-13 22:13:51 -0300

    feat: remove AboutView and refactor HomeView; add HeaderCore component for improved navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+export default {
+  methods: {
+    goToHome() {
+      this.$router.push('/')
+    },
+    goToJobs() {
+      this.$router.push('/catalogo-de-empregos') // Corrigido erro no nome do link
+    },
+  },
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container fluid  class="d-flex justify-center align-center flex-column fill-height text-center bg-secondary">
+    <span class="text-primary text-h3 font-weight-bold">Veja nossas vagas</span>
+    <v-btn
+      class="mt-5 rounded-md px-6"
+      color="primary"
+      variant="outlined"
+      elevation="0"
+      prepend-icon="mdi-arrow-right"
+      @click="goToJobs"
+    >
+      Ir para vagas
+    </v-btn>
+  </v-container>
 </template>
+
+<style>
+
+</style>
